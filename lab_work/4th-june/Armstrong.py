2ecf1ba3d1fbe8dcdf364dd4c7808df7232405ef
abhishek <abhishpal9310@gmail.com>
2026-06-04 15:34:43 +0530

    armstrong change

--- lab_work/4th-june/Armstrong.py
+++ lab_work/4th-june/Armstrong.py
@@ -2,15 +2,16 @@
 
 
 num= int(input("enter any number :"))
-num2=num
-rev = 0
+original_num = num
+n = len(str(num))
+result = 0
 
 while num > 0:
     digit = num % 10
-    rev = rev * 10 + digit
-    num = num // 10
+    result += digit ** n
+    num //= 10
 
-if rev == num2:
-    print(f"{rev} is armstrong")
+if result == original_num:
+    print(f"{original_num} is an Armstrong number.")
 else:
-    print(f"{rev} is not armstrong")
+    print(f"{original_num} is not an Armstrong number.")
--- lab_work/4th-june/electricity_bill.py
+++ lab_work/4th-june/electricity_bill.py
@@ -1,7 +1,30 @@
 # Problem Statement: 
 # Calculate electricity bill based on the following slab rates: 
 
-unit= int(input("enter total unit of consumption"))
-
+unit = int(input("enter total unit of consumption"))
+total = 0 
 if (unit>0 and unit<=100):
+
+    total = unit*5
+
+    print(f"total energy consumption is {unit}")
+    print(f"total bill is {total}")
+
+elif(unit>=101 and unit<=200):
+
+    total = unit*7
+
+    print(f"total energy consumption is {unit}")
+    print(f"total bill is {total}")
+
+elif unit>200:
+
+    total = unit*10
+
+    print(f"total energy consumption is {unit}")
+    print(f"total bill is {total}")
     
+else:
+    print("energy consumption can not be 0 or negative")
+
+
