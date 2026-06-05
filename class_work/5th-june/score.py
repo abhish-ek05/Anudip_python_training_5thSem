565a096be2e9c1862cee3d3344e897d63bca93d6
abhishek <abhishpal9310@gmail.com>
2026-06-05 11:13:10 +0530

    ..

--- class_work/5th-june/score.py
+++ class_work/5th-june/score.py
@@ -2,17 +2,21 @@ score = []
 total = int(input("enter total number of players : "))
 
 if total>0:
+    
     for i in range(1,total+1):
-        a = int(input(f"enter socre of player {i} : "))
+        a = int(input(f"enter score of player {i} : "))
         while a<0:
             print("no negative score !!!")
-            a=int(input(f"enter socre of player {i} : "))
+            a=int(input(f"enter score of player {i} : "))
 
-        else:
-            score.append(a)
-            
-            
+        score.append(a)
+
+
+    print(f"total score is {sum(score)}")                   
+elif total==0:
+    print("score is zero") 
+
 else:
-    print("score is zero")  
+    print("invalid entry") 
+    
 
-print(f"total score is {sum(score)}")
