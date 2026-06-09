dd2ac228946ba3612e2cce907d5e34f5225e2f56
abhishek <abhishpal9310@gmail.com>
2026-06-09 09:53:40 +0530

    password strength alalyser

---
+++ lab_work/8th-june/string/password_strength.py
@@ -0,0 +1,63 @@
+'''A user enters a password. 
+Python@2026! 
+Tasks 
+Write a program to determine whether the password is Strong, Medium, or Weak. 
+Rules: 
+• Minimum length 8  
+• Contains at least:  
+o 1 uppercase letter  
+o 1 lowercase letter  
+o 1 digit  
+o 1 special character  
+Additionally: 
+1. Count uppercase letters.  
+2. Count lowercase letters.  
+3. Count digits.  
+4. Count special characters.  
+5. Display all digits separately.  
+6. Display all special characters separately.  '''
+
+password = "Python@2026!"
+
+upper = 0
+lower = 0
+digits = 0
+special = 0
+
+digit_list = []
+special_list = []
+
+for ch in password:
+    if ch.isupper():
+        upper += 1
+
+    elif ch.islower():
+        lower += 1
+
+    elif ch.isdigit():
+        digits += 1
+        digit_list.append(ch)
+
+    else:
+        special += 1
+        special_list.append(ch)
+
+# Password Strength
+if len(password) >= 8 and upper >= 1 and lower >= 1 and digits >= 1 and special >= 1:
+    strength = "Strong"
+elif len(password) >= 8 and (upper >= 1 or lower >= 1) and digits >= 1:
+    strength = "Medium"
+else:
+    strength = "Weak"
+
+print("Password:", password)
+
+print("\nUppercase Letters:", upper)
+print("Lowercase Letters:", lower)
+print("Digits:", digits)
+print("Special Characters:", special)
+
+print("\nDigits Found:", digit_list)
+print("Special Characters Found:", special_list)
+
+print("\nPassword Strength:", strength)
